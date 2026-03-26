19953bfe3cfb31778a6446b9a558709f70a7b13e
mageshwari05 <mageshwarim9003998405@gmail.com>
2026-03-26 20:08:10 +0530

    Add files via upload

---
+++ if else practice.py
@@ -0,0 +1,28 @@
+print("positive or negative program")
+n=-5
+if (n>0):
+	print("positve")
+else:
+	print("negative")
+print("_____")
+print("vote eligibility program")
+age=5
+if (age>=18):
+	print("eligible to vote")
+else:
+	print("not eligible to vote")
+print("______")
+print("a number divisible by 5 program")
+num=9
+if (num % 5==0):
+	print("divisible by 5")
+else:
+	print("not divisible by 5")
+print("_______")
+print("a number divisible by 5 and 3 program")
+num = 7
+if (num % 5==0 & num % 3==0):
+	print("divisible by 3 and 5")
+else:
+	print("not divisible by 3 and 5")
+
---
+++ var practice 1.py
@@ -0,0 +1,20 @@
+a=5;
+b=10;
+sum=a+b;
+print("a+b:",sum)
+print("--------")
+age=17;
+print(type(age))
+print("---------")
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print("name:",name)
+print("age:",age)
+print("---------")
+a=5;
+b=10;
+print("a,b",b,a)
+print("----")
+name=input("Enter your name:")
+print("hello", name)
+print("-----")
