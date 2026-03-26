6d2b5d63a628d93518b238997ce5bb4bc8f9fd35
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-03-26 21:34:46 +0530

    Add files via upload

---
+++ range practice.py
@@ -0,0 +1,18 @@
+print("PRINT NUMBERS FROM 1-10")
+i=1
+for i in range(1,11):
+    print(i)
+print("PRINT EVEN NUMBERS FROM 1 TO 20")
+i=0
+for i in range(2,22,2):
+    print(i)
+print("PRINT ODD NUMBERS FORM 1 TO 20")
+i=1
+for i in range(1,21,2):
+    print(i)
+print(" MY NAME IN 5 TIMES")
+for i in range(5):
+    print("mageshwari")
+print("MULTIPLICATION OF 2 TABLES")
+for i in range(1,11):
+    print(i,"*2=",2*i)
