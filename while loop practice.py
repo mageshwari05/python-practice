9c5d8cfcfc8caf25434013b046bbaa9b01368769
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-03-27 20:17:40 +0530

    Add files via upload

---
+++ while loop practice.py
@@ -0,0 +1,25 @@
+print("PRINT NUMBERS FORM 1 TO 10")
+i=1
+while (i<=10):
+    print(i)
+    i=i+1
+print("______")
+print("PRINT NUMBERS FROM 10 TO 1")
+i=10
+while (i>=1):
+    print(i)
+    i=i-1
+print("______")
+print("PRINT EVEN NUMBERS FROM 1 TO 20")
+i=2
+while(i<=20):
+    print(i)
+    i=i+2
+print("_______")
+print("PRINT SUM OF NUMBERS FROM 1 TO 10")
+i=1
+sum=0
+while(i<=10):
+    sum=sum+i
+    i=i+1
+print("sum=",sum)
