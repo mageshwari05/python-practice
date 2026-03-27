21b450999a1de9c6f85b31bb72c92d0990bb9f4c
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-03-27 20:27:45 +0530

    Add files via upload

---
+++ variables simple prgm.py
@@ -0,0 +1,13 @@
+c_name="Mageshwari M"
+acc_no=1068
+balance=10000
+acc_active= True
+transactions=["Deposit 1000","withdraw 500","deposit 700"]
+acc_type=("Savings, individuals")
+print("BANK CUSTOMER INFORMATION'S PROGRAM")
+print("CUSTOMER NAME:",c_name)
+print("CUSTOMER ACCOUNT NUMBER:",acc_no)
+print("CUSTOMER BALANCE:",balance)
+print("CUSTOMER ACCOUNT ACCTIVE STATUS (TRUE-1/FALSE-0):",acc_active)
+print("LAST 3 TRANSACTIONS DETAILS:",transactions)
+print("BANK ACCOUNT TYPE:",acc_type)
