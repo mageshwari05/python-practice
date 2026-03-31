0230df4eb5eb827c2ae4d32c0d5f588bb395d1ff
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-03-31 16:40:41 +0530

    Add files via upload

---
+++ list topic.py
@@ -0,0 +1,29 @@
+print("printing the list")
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+print("-----")
+print("access the list elements")
+numbers = [10, 20, 30, 40]
+print(numbers[0])
+print(numbers[2])
+print("-----")
+print("list through loop")
+numbers = [1, 2, 3, 4, 5]
+for i in numbers:
+    print(i)
+print("-----")
+print("Adding the values in a list")
+numbers = [1, 2, 3]
+numbers.append(4)
+print(numbers)
+print("-----")
+print("removing the numbers in list")
+numbers = [1, 2, 3, 4]
+numbers.remove(2)
+print(numbers)
+print("-----")
+print("sorting the numbers")
+numbers = [5, 2, 8, 1]
+numbers.sort()
+print(numbers)
+
