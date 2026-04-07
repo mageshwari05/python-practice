1c017bac14444335d29a04349704b08711cccbf0
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-04-07 19:09:37 +0530

    Add files via upload

---
+++ string prgm.py
@@ -0,0 +1,27 @@
+print("create a string")
+name="python"
+print(name)
+print("-----")
+print("Access characters")
+name = "Python"
+
+print(name[0])
+print(name[1])
+print("-----")
+print("loop through string")
+name = "Python"
+
+for i in name:
+    print(i)
+print("-----")
+print("find length of a string")
+name = "Python"
+print(len(name))
+print("-----")
+print("String functions")
+a = "python"
+print("upppercase:",a.upper())
+a = "PYTHON"
+print("lowercase :",a.lower())
+a = "Python"
+print("replace :",a.replace("P","J"))
