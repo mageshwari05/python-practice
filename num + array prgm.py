edc01fa1a889ad4c25f05815d975dcae2be63d41
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-06-25 13:17:02 +0530

    Add files via upload

---
+++ num + array prgm.py
@@ -0,0 +1,29 @@
+import numpy as np
+# Exercise 1
+print("Exercise 1")
+arr1 = np.array([5, 10, 15, 20, 25])
+print(arr1)
+# Exercise 2
+print("\nExercise 2")
+arr2 = np.array([[10, 20],
+                 [30, 40]])
+print(arr2)
+# Exercise 3
+print("\nExercise 3")
+arr3 = np.arange(1, 21)
+print(arr3)
+# Exercise 4
+print("\nExercise 4")
+arr4 = np.zeros(10)
+print(arr4)
+# Exercise 5
+print("\nExercise 5")
+arr5 = np.ones(10)
+print(arr5)
+# Day 1 Mini Task
+print("\nDay 1 Mini Task")
+marks = np.array([85, 90, 78, 88, 92])
+print("Marks:", marks)
+print("Shape:", marks.shape)
+print("Dimensions:", marks.ndim)
+print("Size:", marks.size)
